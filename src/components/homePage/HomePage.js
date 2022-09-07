0da86bab72b913065762b3e8af482aba23f049bc
fordrodenberg <110043501+fordrodenberg@users.noreply.github.com>
2022-09-07 09:38:11 -0400

    Added carousel to home page

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './HomePage.css'
 import CategoryList from '../categoryList/CategoryList'
+import QuickShopCarousel from '../quickShopCarousel/QuickShopCarousel'
 
 export default function HomePage() {
     return (
         <div>
             <CategoryList />
+            <QuickShopCarousel />
         </div>
     )
 }
