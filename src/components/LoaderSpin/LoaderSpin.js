b3677c72eab5e87c67e2f51bcccea6d9c3cead83
fordrodenberg <110043501+fordrodenberg@users.noreply.github.com>
2022-09-07 15:41:30 -0400

    added a silly loader

---
+++ src/components/LoaderSpin/LoaderSpin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LoaderSpin.css'
+
+export default function LoaderSpin() {
+    return (
+        <div className="loader-spin-root">
+            <div className="circle">
+
+            </div>
+        </div>
+    )
+}
+
