998f0c709288a7bf0182680788f685c880c3a4fa
gotoworq <gotoworq@gmail.com>
2022-09-12 11:44:51 -0400

    made header h2s in checkout form

--- src/components/cart/CheckoutPage/CheckoutPage.js
+++ src/components/cart/CheckoutPage/CheckoutPage.js
@@ -5,18 +5,20 @@ import MultiStepForm from '../../formStep/MultiStepForm'
 
 export default function CheckoutPage() {
     return (
-        <div className='checkout-page'>
-            <div className='info-form'>
-                <h1>
-                    Customer Information
-                </h1>
-                <MultiStepForm />
-            </div>
-            <div className='order-total'>
-                <h1>
-                    Order Total
-                </h1>
-                <OrderTotal />
+        <div>
+            <div className='checkout-page'>
+                <div className='info-form'>
+                    <h2>
+                        Customer Information
+                    </h2>
+                    <MultiStepForm />
+                </div>
+                <div className='order-total'>
+                    <h2>
+                        Order Total
+                    </h2>
+                    <OrderTotal />
+                </div>
             </div>
         </div>
     )
