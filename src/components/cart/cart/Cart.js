54df4983f6cb60fd31289c5b3645fff9600a94ca
gotoworq <gotoworq@gmail.com>
2022-09-14 14:52:14 -0400

    removied cartpreview and put function of that in cart

--- src/components/cart/cart/Cart.js
+++ src/components/cart/cart/Cart.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import './Cart.css'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+
+import UserContext from '../../../hooks/UserContext'
+import { CartContext } from '../../../App'
 import CartList from './cartList/CartList'
 
-
 export default function Cart() {
+    const { activeUser } = useContext(UserContext)
+    var { cart, removeFromCart, updateQuantity } = useContext(CartContext)
+
     return (
-        <div>Cart
-            <Link to="/checkout">
+        <div> Cart
+            <Link to="/checkout" >
                 <button>Checkout</button>
             </Link>
-            <CartList />
-        </div>
+            <CartList items={cart}
+                removeFromCart={removeFromCart}
+                updateQuantity={updateQuantity}
+            />
+        </div >
     )
+
 }
